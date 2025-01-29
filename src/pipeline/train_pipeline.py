d964f41b3cb8adecbe19a1e63a375197f14f929c
SanjayDevarajan03 <sanjay.devarajan02@gmail.com>
2025-01-29 09:20:59 -0500

    Added train_pipeline.py

---
+++ src/pipeline/train_pipeline.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import app
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080, debug=True)
